2649ea25a596af62eaf0c2b4560235b35381cfc1
OleksandrKasapchuk <sasha291108@gmail.com>
2024-02-22 23:14:02 +0200

    added css

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,8 @@ import blog.views as blog_views
 from django.urls import path
 
 urlpatterns = [
-    path("", blog_views.show_posts, name="post_list"),
-    path("<int:post_id>/", blog_views.get_post, name="post_details")
+    path("posts/", blog_views.show_posts, name="post_list"),
+    path("posts/<int:post_id>/", blog_views.get_post, name="post_details"),
+    path("authors/", blog_views.show_authors, name="author_list"),
+    path("authors/<int:author_id>/", blog_views.get_author, name="author_posts")
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from blog.models import Post
+from blog.models import Post, Author
 # Create your views here.
 def show_posts(request):
     posts = Post.objects.all()
@@ -11,9 +11,27 @@ def show_posts(request):
     )
 def get_post(request, post_id):
     post = Post.objects.get(id=post_id)
-    context = {"post": post}
+    author = Author.objects.get(id=post.author_id)
+    context = {"post": post, "author":author}
     return render(
         request,
         template_name="blog/post_details.html",
         context = context
+    )
+def show_authors(request):
+    authors = Author.objects.all()
+    context = {"authors": authors}
+    return render(
+        request,
+        template_name="blog/authors.html",
+        context=context
+    ) 
+def get_author(request, author_id):
+    author = Author.objects.get(id=author_id)
+    author_posts = Post.objects.filter(author_id=author_id)
+    context = {"author": author, "author_posts": author_posts}
+    return render(
+        request,
+        template_name="blog/author_posts.html",
+        context=context
     )
